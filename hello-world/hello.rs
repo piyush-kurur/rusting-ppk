69671ff827b0133f24b86a63e06ed26c8df10181
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-10-12 19:11:44 +0530

    hello world I am here.

---
+++ hello-world/hello.rs
@@ -0,0 +1,16 @@
+/*
+
+This is the first program that I am writing in rust. As an offering to
+the K&R gods I write my first hello world program. Just noticed that
+the C style comments work. Let me try the line comments.
+
+
+Let me commit this code now.
+
+*/
+
+
+fn main ()
+{
+    println!("hello world")
+}
